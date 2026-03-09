541ce96d95700ce5129873704ed084f4e7a0c887
nqobih-wemvula <nqobihmthembu@gmail.com>
2026-03-09 15:25:24 +0200

    Refactor Anagrams class and fix method signatures
    Refactor Anagrams class methods for better functionality and fix syntax errors.

--- Anagrams.java
+++ Anagrams.java
@@ -5,36 +5,48 @@ import java.util.*;
 
 
 
-public Anagrams{
-  static void signature(word){
-    return ''.join(sorted(word));
+public anas{
+  static String signature( String word){
+    char[] chars = s.toCharArray();
+    Arrays.sort(chars);
+    return String(chars);
   }
-  static void System(command){
+  static int System(String command){
     System.out.println("-"*50 + ">", command);
-      String s = system(command);
-    return s;
-  }
+      try {
+            Process process = Runtime.getRuntime().exec(command);
+            return process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+}
   public static void main(String[] args){
+     if (args.length != 1) {
+            System.out.println("Usage: java Anagrams inputfile.\n You gave:\n " + Arrays.toString(args));
+            System.exit(1);
+        }
+
+        String inputfile = args[0];
+        System.out.println("Data file: " + inputfile);
+
+        Map<String, Integer> D = new HashMap<>();
+        int linenumber = 0;
+        int lines = 0;
+
 
     try(BufferedReader file = new BufferedReader(new FIleReader(){
-      String text = file.readLine();
-      int lines = 0;
-      int linenumber= 0;
-      while( text != ""){
+         while( String text == file.readLine()!= ""){
         linenumber += 1;
-       String[] words = text.split();
+       String[] words = text.split("\\s+");
         for(String w : words){
-          w = w.strip([0123456789(,.,.;:_.!?---)]');
-          w = w[:];
-          if( w : D){
-            D[W] += 1;
-          }
-          else{
-            D[W] = 1;
-          }
-        }
-        text = file.readLine();
-        lines = += 1;
+          String W = w.replaceAll([0123456789(,.,.;:_.!?---)]');
+          if (!W.isEmpty()) {
+                        D.put(W, D.getOrDefault(W, 0) + 1);
+                    }
+                }
+                lines++;
+
       
       }
         file.close();
@@ -70,9 +82,10 @@ public Anagrams{
        for(String r =0 ; r< (A[Key]).length ; r++ ){
          String space = anagramList.find(' ');
          anagramlist = anagramlist[space +1:] + ' ' + anagramlist[: space];
-         System.out.printl
+         System.out.println(anagramlist, end = "\\\\\n", File = file);
        }
      }
    }
+    file.close();
   }
 }
